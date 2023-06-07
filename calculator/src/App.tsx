79fe9645cae49f658188a40f46cb875d3a8baa7f
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-06-07 16:09:51 +0300

    feat: add functional (#1)

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -15,9 +15,58 @@ import {
 } from './App.style';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [theme, setTheme] = useState(1);
+  const [themeValue, setThemeValue] = useState('8%');
+  console.log(value.length);
+
+  const handleTheme = () => {
+    if (theme === 1) {
+      setTheme(2);
+      setThemeValue('38%');
+    } else if (theme === 2) {
+      setTheme(3);
+      setThemeValue('70%');
+    } else {
+      setTheme(1);
+      setThemeValue('8%');
+    }
+  };
+
+  const deleteValue = () => {
+    if (value.slice(-1) === ' ') {
+      setValue(value.substring(0, value.length - 3));
+    } else if (value.slice(-2) === '0.') {
+      setValue(value.substring(0, value.length - 2));
+    } else {
+      setValue(value.substring(0, value.length - 1));
+    }
+  };
+
+  const addSymbol = (param: string) => {
+    if (value.slice(-1) !== ' ' && value.slice(-1) !== '.') {
+      if (value.includes('.')) {
+        return false;
+      }
+      setValue(value + param);
+    }
+  };
+
+  const calculate = () => {
+    if (value.length >= 5 && value.slice(-1) !== ' ') {
+      setValue(eval(value).toString());
+    }
+  };
+
+  const reset = () => {
+    setValue('');
+  };
+
   return (
     <>
-      {<GlobalStyle1 />}
+      {theme === 1 && <GlobalStyle1 />}
+      {theme === 2 && <GlobalStyle2 />}
+      {theme === 3 && <GlobalStyle3 />}
       <Container>
         <Header>
           Calculator React
@@ -29,46 +78,62 @@ function App() {
                 <span>2</span>
                 <span>3</span>
               </div>
-              <SwitchContainer>
-                <Switcher />
+              <SwitchContainer onClick={handleTheme}>
+                <Switcher theme={themeValue} />
               </SwitchContainer>
             </Switch>
           </WrapperSwitch>
         </Header>
-        <Input></Input>
+        <Input>{value}</Input>
         <ButtonContainer>
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>DEL</Button>
+          <Button onClick={() => setValue(value + '7')}>7</Button>
+          <Button onClick={() => setValue(value + '8')}>8</Button>
+          <Button onClick={() => setValue(value + '9')}>9</Button>
+          <Button
+            onClick={() => value.length >= 1 && deleteValue()}
+            color="var(--white)"
+            bg="var(--key-background-dark-blue)"
+            bdbox="var(--key-shadow-dark-blue)">
+            DEL
+          </Button>
 
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>+</Button>
+          <Button onClick={() => setValue(value + '4')}>4</Button>
+          <Button onClick={() => setValue(value + '5')}>5</Button>
+          <Button onClick={() => setValue(value + '6')}>6</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' + ')}>
+            +
+          </Button>
 
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>-</Button>
+          <Button onClick={() => setValue(value + '1')}>1</Button>
+          <Button onClick={() => setValue(value + '2')}>2</Button>
+          <Button onClick={() => setValue(value + '3')}>3</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' - ')}>
+            -
+          </Button>
 
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button>/</Button>
-          <Button>x</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol('.')}>.</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol('0')}>0</Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' / ')}>
+            /
+          </Button>
+          <Button onClick={() => value.length >= 1 && addSymbol(' * ')}>
+            x
+          </Button>
 
           <Button
             gc="1/3"
             color="var(--white)"
             bg="var(--key-background-blue)"
-            bdbox="var(--key-shadow-dark-blue)">
+            bdbox="var(--key-shadow-dark-blue)"
+            onClick={reset}>
             RESET
           </Button>
           <Button
             gc="3/5"
             color="var(--white)"
             bg="var(--key-background-red)"
-            bdbox="var(--key-shadow-dark-blue)">
+            bdbox="var(--key-shadow-dark-blue)"
+            onClick={calculate}>
             =
           </Button>
         </ButtonContainer>
